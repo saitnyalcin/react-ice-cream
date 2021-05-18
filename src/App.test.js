3d291b8d36153f786a1d041850490f959cf228b9
Steve Yalcin <steve.yalcin@ibm.com>
2021-05-18 18:35:00 -0400

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("should render the application title", () => {
+  render(<App />);
+  const appTitle = screen.getByText(/react application/i);
+  expect(appTitle.textContent).toBe("React Application");
 });
